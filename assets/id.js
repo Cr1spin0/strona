d328e9e333ea6d2d045382c3c935dee7ba955439
Cr1spin0 <77028298+Cr1spin0@users.noreply.github.com>
2025-03-31 16:21:15 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -3,3 +3,15 @@ if ("serviceWorker" in navigator) {
     console.log("Service Worker zarejestrowany!");
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const helpImg = document.querySelector(".help_img");
+    const helpBox = document.getElementById("helpBox");
+
+    helpImg.addEventListener("click", function () {
+        if (helpBox.style.display === "none" || helpBox.style.display === "") {
+            helpBox.style.display = "block";
+        } else {
+            helpBox.style.display = "none";
+        }
+    });
+});
